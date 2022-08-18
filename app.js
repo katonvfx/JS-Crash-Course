48c54794dc905d7a62978d95ab91133a1a795160
katonvfx <maiatube123@gmail.com>
2022-08-19 01:36:27 +1000

    First Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+// console. log ('Hello World'[10])
+/**
+ * Converting Temperatures
+ * 
+ * Celsius-to-Farenheit formula:
+ * 
+ *   F = C x 1.8 + 32
+ */
+
+// let celsius = 10;
+// let Farenheit = celsius * 1.8 + 32;
+
+// console. log (Farenheit)
+
+/**IF/ELSE CONDITIONALS*/
+
+// let cash = 55
+// let price = 40
+// let difference = cash - price
+
+// if (cash > price) {
+//     console. log (`you paid extra - here is ${difference} dollars change`)
+// }
+
+// else if (cash === price) {
+//     console. log ("you paid the exact ammount - have a great day!")
+// }
+
+// else{
+//     console. log (`not enough money - you owe ${difference * -1} dollars`)
+// }
+
+
+/**LOGICAL OPERATORS*/
+
+
+// let cash = 50
+// let price = 40
+// let isStoreOpen = true
+
+
+// let str = cash >= price && isStoreOpen ? 'give reciept' : 'dont give receipt'
+
+// console. log (str)
+
+/**LOOPS */
+
+// while (count <=100) {
+//     console. log (count)
+//     count = count +1;
+// }
+
+// for (let i = 0; i < 100; i++) {
+//     console. log(i +)
+
+// }
+
+// for (i = 1; i <= 20; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console. log(`${i} -> Frontend Simplified`)
+//     }
+
+//     else if (i % 3 === 0 ) {
+//         console. log(`${i} ->Frontend`)
+
+//     }
+    
+//     else if (i % 5 === 0) {
+//         console. log(`${i} ->Simplified`)
+//     }
+
+//     else {
+//         console. log(`${i} -> ${i}`)
+//     }
+// }
+
+// const str = "Frontend Simplified"
+// for (i = 0; i < str.length; i ++){
+//     console. log(str[i])
+// }
+
+/**FUNCTIONS */
+
+
+    
+
+
+
+
+
+ 
